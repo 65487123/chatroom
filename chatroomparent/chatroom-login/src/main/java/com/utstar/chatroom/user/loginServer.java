4bc1247045c2a6896ecd2de89ca79666910feb40
Lu Zeping <zeping.lu@utstar.com>
2020-01-17 17:29:22 +0800

    delete document

---
+++ chatroomparent/chatroom-login/src/main/java/com/utstar/chatroom/user/loginServer.java
@@ -0,0 +1,14 @@
+package com.utstar.chatroom.user;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author：luzeping
+ * @Date: 2020/1/17 17:16
+ */
+@SpringBootApplication
+public class loginServer {
+    public static void main(String[] args) {
+
+    }
+}
