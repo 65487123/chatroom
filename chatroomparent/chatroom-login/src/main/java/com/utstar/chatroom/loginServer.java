99846719f966f19eb93ad77eaa2900ef2f2498be
65487123 <382513254@qq.com>
2020-01-17 23:36:53 +0800

    add

--- chatroomparent/chatroom-login/src/main/java/com/utstar/chatroom/loginServer.java
+++ chatroomparent/chatroom-login/src/main/java/com/utstar/chatroom/loginServer.java
@@ -1,5 +1,10 @@
 package com.utstar.chatroom;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -8,6 +13,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class loginServer {
+    /**
+     * description:
+     * @Author：luzeping
+    
+     * @return void
+     */
+    private void startServer(int port){
+        EventLoopGroup bossEventLoopGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerEventLootGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.channel(NioServerSocketChannel.class).group(bossEventLoopGroup, workerEventLootGroup).option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true).childHandler(new ServerInitializer());
+            bootstrap.bind(port).sync().channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+        } finally {
+            bossEventLoopGroup.shutdownGracefully();
+            workerEventLootGroup.shutdownGracefully();
+        }
+    }
     public static void main(String[] args) {
 
     }
